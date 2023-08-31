fef626be2d6d575d9f60b12818dcb23970cd1aea
marukun712 <marukun530@gmail.com>
2023-09-01 08:38:31 +0900

    ✨serverを追加

---
+++ server.js
@@ -0,0 +1,10 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+
+app.use(async (ctx) => {
+    let json = await Deno.readTextFile("./res.json");
+    ctx.response.body = json;
+})
+
+await app.listen({ port: 8000 });
